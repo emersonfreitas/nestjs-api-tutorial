bf564004c30b3037d1abca4246f92cef0d1316a8
Emerson Freitas <emersonfreitasn@gmail.com>
2022-07-19 08:54:51 -0500

    auth controller e authe service

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable({})
+export class AuthService {
+  signUp() {
+    return 'I am signed up';
+  }
+
+  signIn() {
+    return 'I am signed in';
+  }
+}
